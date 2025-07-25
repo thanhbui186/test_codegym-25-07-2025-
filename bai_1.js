9044a1347dbb49ca0400b81d2a99234bd054eac7
thanhbui186 <thanh1862001@gmail.com>
2025-07-25 21:35:55 +0700

    bai_1.js

--- bai_1.js
+++ bai_1.js
@@ -3,7 +3,7 @@ function snt(n){
     if (n === 2) return true;
     if (n % 2 === 0) return false;
 
-    for (let i= 3; i*i <= n; i += 2){
+    for (let i = 3; i*i <= n; i += 2){
         if (n % i===0) return false;
     }
     return true;
